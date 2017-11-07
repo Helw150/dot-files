f6e7abd2d9a7515e569dfd23e5ea86be0ea9a8f2
Helw150 <wbh230@nyu.edu>
2017-11-07 15:02:23 -0500

    Tmux and Crosh styling

---
+++ zenburn.js
@@ -0,0 +1,60 @@
+/*
+This Javascript sets the Chrome OS Shell to utilize the Zenburn color theme
+*/
+// Disable bold.
+term_.prefs_.set('enable-bold', false)
+
+// Use this for Zenburn
+term_.prefs_.set('background-color', "#3F3F3F");
+term_.prefs_.set('foreground-color', "#DCDCCC");
+
+base03 =     "#002b36";
+base02 =     "#073642";
+base01 =     "#586e75";
+base00 =     "#657b83";
+base0 =      "#839496";
+base1 =      "#93a1a1";
+base2 =      "#eee8d5";
+base3 =      "#fdf6e3";
+yellow =     "#F0DFAF";
+orange =     "#DFAF8F";
+red =        "#CC9393";
+magenta =    "#DC8CC3";
+violet =     "#806080";
+blue =       "#8CD0D3";
+cyan =       "#93E0E3";
+green =      "#7F9F7F";
+
+term_.prefs_.set('color-palette-overrides',
+		 [ base02 , red     , green  , yellow,
+		   blue     , magenta , cyan   , base2,
+		   base03   , orange  , base01 , base00,
+		   base0    , violet  , base1  , base3 ]);// Disable bold.
+term_.prefs_.set('enable-bold', false)
+
+// Use this for Zenburn
+term_.prefs_.set('background-color', "#3F3F3F");
+term_.prefs_.set('foreground-color', "#DCDCCC");
+
+base03 =     "#002b36";
+base02 =     "#073642";
+base01 =     "#586e75";
+base00 =     "#657b83";
+base0 =      "#839496";
+base1 =      "#93a1a1";
+base2 =      "#eee8d5";
+base3 =      "#fdf6e3";
+yellow =     "#F0DFAF";
+orange =     "#DFAF8F";
+red =        "#CC9393";
+magenta =    "#DC8CC3";
+violet =     "#806080";
+blue =       "#8CD0D3";
+cyan =       "#93E0E3";
+green =      "#7F9F7F";
+
+term_.prefs_.set('color-palette-overrides',
+		 [ base02 , red     , green  , yellow,
+		   blue     , magenta , cyan   , base2,
+		   base03   , orange  , base01 , base00,
+		   base0    , violet  , base1  , base3 ]);
